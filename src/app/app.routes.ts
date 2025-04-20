b21c3e4d3c7c3475604b4c3a197a0c0cf08f5e9d
Natavan Abasova <ntabasova@gmail.com>
2025-04-20 19:31:04 +0200

    set routing and create thanks page (svg animation will be added)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { AhoyPageComponent } from './ahoy-page/ahoy-page.component';
+import { ThanksPageComponent } from './thanks-page/thanks-page.component';
 
-export const routes: Routes = [{ path: '', component: AhoyPageComponent }];
+export const routes: Routes = [
+  { path: '', component: AhoyPageComponent },
+  { path: 'thanks', component: ThanksPageComponent },
+];
