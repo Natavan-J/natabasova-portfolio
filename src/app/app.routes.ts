4e40dbc9aee0402a5623a5021cf3499e37df20c6
Natavan Abasova <ntabasova@gmail.com>
2025-04-21 01:10:15 +0200

    projects page layout completed, other pages components created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { AhoyPageComponent } from './ahoy-page/ahoy-page.component';
 import { ThanksPageComponent } from './thanks-page/thanks-page.component';
+import { ProjectsPageComponent } from './projects-page/projects-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
 
 export const routes: Routes = [
   { path: '', component: AhoyPageComponent },
+  { path: 'about', component: AboutPageComponent },
+  { path: 'projects', component: ProjectsPageComponent },
   { path: 'thanks', component: ThanksPageComponent },
 ];
